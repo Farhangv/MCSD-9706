f37892cec34c3b9590c8311e5b1005b605109bb1
Farhang Vafa <farhangv@ymail.com>
2018-10-03 19:53:20 +0330

    970711

---
+++ Session10-970711/IEnumerableDemo/Exam.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IEnumerableDemo
+{
+    class Exam:IEnumerable
+    {
+        public Question[] Questions { get; set; }
+
+        public IEnumerator GetEnumerator()
+        {
+            //return Questions.GetEnumerator();
+            return new ExamEnumerator(Questions);
+
+        }
+    }
+
+    class ExamEnumerator : IEnumerator
+    {
+        private Question[] questions;
+
+        public ExamEnumerator(Question[] _questions)
+        {
+            questions = _questions;
+        }
+
+        private int currentIndex = -1;
+        public object Current
+        {
+            get
+            {
+                return questions[currentIndex];
+            }
+        }
+
+        public bool MoveNext()
+        {
+            currentIndex += 2;
+            return currentIndex < questions.Length;
+        }
+
+        public void Reset()
+        {
+            currentIndex = -1;
+        }
+    }
+}
---
+++ Session10-970711/IEnumerableDemo/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IEnumerableDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Exam exam = new Exam();
+            exam.Questions = new Question[]
+                {
+                    new Question() { Text = "What's your name?" },
+                    new Question() { Text = "Describe OOP" },
+                    new Question() { Text = "Describe GC" },
+                    new Question() { Text = "What's the difference between Interface and Abstract Class?" }
+                };
+
+            foreach (Question question in exam)
+            {
+                Console.WriteLine(question.Text);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
