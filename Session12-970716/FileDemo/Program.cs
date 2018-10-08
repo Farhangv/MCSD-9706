417bdc7f9198bb883b24ecfd90e831e7cbd29899
Farhang Vafa <farhangv@ymail.com>
2018-10-08 19:56:49 +0330

    970716

---
+++ Session12-970716/FileDemo/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace FileDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ////نوشتن
+            //using (StreamWriter sw = new StreamWriter(@"myfile.txt", true))
+            //{
+            //    //sw.AutoFlush = true;
+            //    while (true)
+            //    {
+            //        var input = Console.ReadLine();
+            //        if (input == "exit")
+            //            break;
+            //        sw.WriteLine(input);
+            //        sw.Flush();
+            //    }
+            //}//sw.Close();
+
+            //خواندن
+            
+            using (StreamReader sr = new StreamReader("myfile.txt"))
+            {
+                //while (sr.Peek() != -1)
+                //{
+                //    Console.Write((char)sr.Read());
+                //}
+
+                //while (sr.Peek() != -1)
+                //{
+                //    Console.WriteLine(sr.ReadLine());
+                //}
+
+                Console.WriteLine(sr.ReadToEnd());
+            }
+
+            
+            Console.ReadKey();
+        }
+    }
+}
