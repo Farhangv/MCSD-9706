108929d9e8422444351764d007c0b63491e7dea4
Farhang Vafa <farhangv@ymail.com>
2018-10-29 20:03:25 +0330

    970807

---
+++ Session22-970807/WebAppDemo/Scripts/js-demo.js
@@ -0,0 +1,40 @@
+﻿//let names = new Array();
+let names = ['C#', 'Java'];
+
+console.log(names[0]);
+names[2] = "PHP"; 
+
+names.push('Python');
+names.push('Ruby');
+names.push('TypeScript');
+names.unshift('C++');
+
+
+console.log(`pop(): ${names.pop()}`);
+console.log(`shift(): ${names.shift()}`);
+
+names.splice(2, 1, "R", "Go");
+
+
+console.log('------------------------');
+for (var i = 0; i < names.length; i++) {
+    console.log(names[i]);
+}
+
+console.log('------------------------');
+for (let ix in names) {
+    console.log(`[${ix}] : ${names[ix]}`);
+}
+
+console.log('------------------------');
+for (let name of names) {
+    console.log(name);
+}
+
+console.log('------------------------');
+console.log(typeof names);
+console.log(names instanceof Array);
+console.log(Array.isArray(names));
+
+console.log('------------------------');
+console.log(names.join(','));
