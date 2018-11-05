fb02a323566ef228e80c40d97259f9e6d553f3cd
Farhang Vafa <farhangv@ymail.com>
2018-11-05 19:49:03 +0330

    970814

---
+++ Session25-970814/MVCDemo/Controllers/DemoController.cs
@@ -0,0 +1,29 @@
+﻿using MVCDemo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCDemo.Controllers
+{
+    public class DemoController : Controller
+    {
+        public ActionResult Sample()
+        {
+            //return Content("سلام");
+            //return View();
+            //Person p = new Person() { Name = "رضا", Family = "رضایی" };
+            //return View(p);
+            //return new ContentResult() { Content = "سلام", ContentEncoding = Encoding.UTF8, ContentType = "plain/text" };
+            return new EmptyResult();
+        }
+
+        public ActionResult ViewDemo()
+        {
+            //return View("~/Views/MyFolder/MyView.cshtml");
+            return View();
+        }
+    }
+}
