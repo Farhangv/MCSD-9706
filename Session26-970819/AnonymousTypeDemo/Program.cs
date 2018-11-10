3a2556733ad151db894ab7da510fb53e272bb075
Farhang Vafa <farhangv@ymail.com>
2018-11-10 19:35:11 +0330

    970819

---
+++ Session26-970819/AnonymousTypeDemo/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnonymousTypeDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var p = new { Name = "John", Family = "Doe" };
+
+            Console.WriteLine(p.Name);
+            Console.WriteLine(p.Family);
+            //Console.WriteLine(p.Age);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Session26-970819/DynamicObjectDemo/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynamicObjectDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            dynamic p = new ExpandoObject();
+            p.Name = "John";
+            p.Family = "Doe";
+
+            Console.WriteLine(p.Name);
+            Console.WriteLine(p.Family);
+            Console.WriteLine(p.Age);
+
+            Console.ReadKey();
+            
+        }
+    }
+
+    class Person {
+        public string Name { get; set; }
+        public string Family { get; set; }
+    }
+}
