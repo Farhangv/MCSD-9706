c8933ec3482a86115f01f729341566afd2f7f5dd
Farhang Vafa <farhangv@ymail.com>
2018-11-12 18:26:17 +0330

    970821

---
+++ Session27-970821/MVCDemo/Repository/PeopleRepository.cs
@@ -0,0 +1,26 @@
+﻿using MVCDemo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCDemo.Repository
+{
+    public class PeopleRepository
+    {
+        public List<Person> GetAllPeople()
+        {
+            return new List<Person>() {
+                new Person() { Name = "رضا", Family = "رضایی" },
+                new Person() { Name = "مریم", Family = "مریمی" },
+                new Person() { Name = "پیام", Family = "پیامی" }
+            };
+        }
+
+        public bool AddPerson(Person person)
+        {
+            //Add Person To DB
+            return true;
+        }
+    }
+}
