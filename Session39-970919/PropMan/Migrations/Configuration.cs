4f93276c6be65954eab0a56f39983e41a3389360
Farhang Vafa <farhangv@ymail.com>
2018-12-10 20:13:20 +0330

    970919

---
+++ Session39-970919/PropMan/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+namespace PropMan.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<PropMan.Models.PropManDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(PropMan.Models.PropManDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
