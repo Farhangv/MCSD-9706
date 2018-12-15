6d3ad37d76ca1d4a37639dab3bc1d3f826fa37c6
Farhang Vafa <farhangv@ymail.com>
2018-12-15 19:54:12 +0330

    970924

---
+++ Session41-970924/PropMan/Models/Location.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace PropMan.Models
+{
+    [Table("Location")]
+    public class Location
+    {
+        public int Id { get; set; }
+        [MaxLength(50), Display(Name = "کشور")]
+        public string Country { get; set; }
+        [MaxLength(50), Display(Name = "شهر")]
+        public string City { get; set; }
+        [MaxLength(100), Display(Name = "محله")]
+        public string District { get; set; }
+
+        public virtual ICollection<Property> Properties { get; set; }
+    }
+}
---
+++ Session41-970924/PropMan/ViewModels/PropertyCreateViewModel.cs
@@ -0,0 +1,41 @@
+﻿using PropMan.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PropMan.ViewModels
+{
+    public class PropertyCreateViewModel
+    {
+        [MaxLength(50), Display(Name = "عنوان")]
+        public string Title { get; set; }
+        [Display(Name = "مساحت")]
+        public int Area { get; set; }
+        [Display(Name = "تعداد خواب")]
+        public int BedRoomsCount { get; set; }
+        [Display(Name = "موقعیت واحد")]
+        public HousePosition Position { get; set; }
+        [Display(Name = "سال ساخت")]
+        public int Age { get; set; }
+        [Display(Name = "تاریخ اعتبار")]
+        public string ValidUntilPersianDate { get; set; }
+        [Display(Name = "نوع آگهی")]
+        public AdType AdType { get; set; }
+        [Display(Name = "قیمت هر متر مربع")]
+        public int? SaleUnitPrice { get; set; }
+        [Display(Name = "رهن")]
+        public int? RentDiposite { get; set; }
+        [Display(Name = "اجاره ماهانه")]
+        public int? RentMonthly { get; set; }
+        [Display(Name = "منطقه")]
+        public int LocationId { get; set; }
+        [Display(Name = "نوع ملک")]
+        public int PropertyTypeId { get; set; }
+
+        [Display(Name = "توضیحات"),AllowHtml]
+        public string Description { get; set; }
+    }
+}
