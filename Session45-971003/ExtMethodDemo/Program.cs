35ed1c19cb89c15f71508c288a6d6b17d87fa623
Farhang Vafa <farhangv@ymail.com>
2018-12-24 20:14:09 +0330

    971003

---
+++ Session45-971003/ExtMethodDemo/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtMethodDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = StringExtensions.ToInt("123");
+            int b = "123".ToInt();
+        }
+    }
+}
