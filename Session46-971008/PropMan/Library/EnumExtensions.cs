893ff66d45e7a46c1a93b7b92b870c6c6463d947
Farhang Vafa <farhangv@ymail.com>
2018-12-29 19:46:06 +0330

    Session46-971008

---
+++ Session46-971008/PropMan/Library/EnumExtensions.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PropMan.Library
+{
+    public static class EnumExtensions
+    {
+        public static HtmlString GetDisplayName(this Enum enumValue)
+        {
+            return 
+                new HtmlString(
+                 enumValue.GetType()
+                .GetMember(enumValue.ToString())
+                .First()
+                .GetCustomAttribute<DisplayAttribute>()
+                .GetName());
+        }
+
+        public static HtmlString GetEnumDisplayName(this HtmlHelper html, Enum enumValue)
+        {
+            return
+                new HtmlString(
+                 enumValue.GetType()
+                .GetMember(enumValue.ToString())
+                .First()
+                .GetCustomAttribute<DisplayAttribute>()
+                .GetName());
+        }
+    }
+}
---
+++ Session46-971008/SessionCookieDemo/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SessionCookieDemo.MvcApplication" Language="C#" %>
